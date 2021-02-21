ae96920b070176576b4c82a02b020b85a1764aae
Matthew Layton <matthew.layton@live.co.uk>
2021-02-21 15:16:50 +0000

    Preparation for v1.0.0

--- src/main/kotlin/io/onixlabs/kotlin/core/Extensions.Int.kt
+++ src/main/kotlin/io/onixlabs/kotlin/core/Extensions.Int.kt
@@ -16,12 +16,29 @@
 
 package io.onixlabs.kotlin.core
 
-/**
- * Provides the constant value for unchecked cast suppression.
- */
-internal const val UNCHECKED_CAST = "UNCHECKED_CAST"
+import java.math.BigDecimal
+import java.math.BigInteger
 
 /**
- * Provides the constant value for unused member suppression.
+ * Gets the constant ZERO of type [Int].
  */
-internal const val UNUSED = "UNUSED"
+val Int.Companion.ZERO: Int get() = 0
+
+/**
+ * Gets the constant ONE of type [Int].
+ */
+val Int.Companion.ONE: Int get() = 1
+
+/**
+ * Converts an [Int] value to a [BigInteger] value.
+ *
+ * @return Returns a [BigInteger] representation of this [Int] value.
+ */
+fun Int.toBigInteger(): BigInteger = BigInteger.valueOf(toLong())
+
+/**
+ * Converts an [Int] value to a [BigDecimal] value.
+ *
+ * @return Returns a [BigDecimal] representation of this [Int] value.
+ */
+fun Int.toBigDecimal(): BigDecimal = BigDecimal.valueOf(toLong())
--- src/main/kotlin/io/onixlabs/kotlin/core/Extensions.Long.kt
+++ src/main/kotlin/io/onixlabs/kotlin/core/Extensions.Long.kt
@@ -16,12 +16,29 @@
 
 package io.onixlabs.kotlin.core
 
-/**
- * Provides the constant value for unchecked cast suppression.
- */
-internal const val UNCHECKED_CAST = "UNCHECKED_CAST"
+import java.math.BigDecimal
+import java.math.BigInteger
 
 /**
- * Provides the constant value for unused member suppression.
+ * Gets the constant ZERO of type [Long].
  */
-internal const val UNUSED = "UNUSED"
+val Long.Companion.ZERO: Long get() = 0L
+
+/**
+ * Gets the constant ONE of type [Long].
+ */
+val Long.Companion.ONE: Long get() = 1L
+
+/**
+ * Converts a [Long] value to a [BigInteger] value.
+ *
+ * @return Returns a [BigInteger] representation of this [Long] value.
+ */
+fun Long.toBigInteger(): BigInteger = BigInteger.valueOf(this)
+
+/**
+ * Converts a [Long] value to a [BigDecimal] value.
+ *
+ * @return Returns a [BigDecimal] representation of this [Long] value.
+ */
+fun Long.toBigDecimal(): BigDecimal = BigDecimal.valueOf(this)
